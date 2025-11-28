2e239620e94385c8f7495db35f64759dabcefff1
iheanyi.okwara <okwaraiheanyi@gmail.com>
2025-11-28 21:30:41 +0000

    Performing Arithmetic Operations

--- Lessons/app.py
+++ Lessons/app.py
@@ -1159,7 +1159,7 @@ print(point)
 '''
 # Comparing Objects:
 
-
+'''
 class Point:
     def __init__(self, x, y):
         self.x = x
@@ -1175,3 +1175,22 @@ class Point:
 point = Point(10, 20)
 other = Point(1, 2)
 print(point < other)
+
+'''
+
+# Supporting Arithmetic Operations
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
+
+point = Point(10, 20)
+other = Point(1, 2)
+combined = point + other
+print(combined.x)
