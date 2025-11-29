9a8d13f8abd6a4c0eeea7d11e0608dc5b15f1e24
iheanyi.okwara <okwaraiheanyi@gmail.com>
2025-11-29 06:52:49 +0000

    Creating custom containers

--- Lessons/app.py
+++ Lessons/app.py
@@ -1180,7 +1180,7 @@ print(point < other)
 
 # Supporting Arithmetic Operations
 
-
+'''
 class Point:
     def __init__(self, x, y):
         self.x = x
@@ -1194,3 +1194,49 @@ point = Point(10, 20)
 other = Point(1, 2)
 combined = point + other
 print(combined.x)
+
+'''
+
+#  Making Custom Containers:
+
+
+# With this class we can keep track of number of various tags on a blog
+
+# +class TagCloud:
+'''
+Thigs we can do with the TagCloud
+
+cloud = TagCloud()
+len(cloud)
+cloud["python"] = 10
+for tag in cloud:
+    print(tag)
+
+'''
+
+
+class TagCloud:
+    def __init__(self):
+        self.tags = {}
+
+    def add(self, tag):
+        self.tags[tag.lower()] = self.tags.get(tag.lower(), 0) + 1
+
+    def __getitem__(self, tag):
+        return self.tags.get(tag.lower(), 0)
+
+    def __setitem__(self, tag, count):
+        self.tags[tag.lower()] = count
+
+    def __len__(self):
+        return len(self.tag)
+
+    def __iter__(self):
+        return iter(self.tags)
+
+
+cloud = TagCloud()
+cloud.add("Python")
+cloud.add("python")
+cloud.add("python")
+print(cloud.tags)
